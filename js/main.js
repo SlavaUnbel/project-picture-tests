d6b765a9d426914c489b73450dc81d4afdb71c74
Unbelievable <slavlen1999@gmail.com>
2019-02-04 21:42:30 +0200

    add some features about scrolling and sliders in main.js

--- js/main.js
+++ js/main.js
@@ -22,6 +22,122 @@
 				$('#preloader').delay(300).fadeOut('slow');
 			});
 		});
-	};
-	
+	}
+
+	/* FitVids */
+	const ssFitVids = () => {
+		$('.fluid-video-wrapper').fitVids();
+	}
+
+	/* Masonry */
+	const ssMasonryFolio = () => {
+
+		const containerBricks = $('.bricks-wrapper');
+
+		containerBricks.imageLoaded( () => {
+			containerBricks.masonry( {
+				itemSelector: '.brick',
+				resize: true
+			});
+		});
+	}
+
+	/* Light Gallery */
+	const ssLightGallery = () => {
+		$('#folio-wrap').lightGallery( {
+			showThumbByDefault: false,
+			hash: false,
+			selector: '.item-wrap'
+		});
+	}
+
+	/* Flexslider */
+	const ssFlexSlider = () => {
+
+		$WIN.on('load', () => {
+			$('#testimonial-slider').flexSlider( {
+				namespace: 'flex-',
+				controlsContainer: '',
+				animation: 'slide',
+				controlNav: true,
+		      	directionNav: false,
+		      	smoothHeight: true,
+		      	slideshowSpeed: 7000,
+		      	animationSpeed: 600,
+		      	randomize: false,
+		      	touch: true,
+			});
+		});
+	}
+
+	/* Carousel */
+	const ssOwlCarousel = () => {
+		$('.owl-carousel').owlCarousel( {
+			nav: false,
+			loop: true,
+			margin: 50,
+			responsiveClass: true,
+			responsive: {
+				0:{
+	            	items:2,
+	            	margin: 20
+	         	},
+	         	400:{
+	            	items:3,
+	            	margin: 30
+	         	},
+	         	600:{
+	            	items:4,
+	            	margin: 40
+	         	},
+	         	1000:{
+	            	items:6            
+	         	}
+			}
+		});
+	}
+
+	/* Menu on Scrolldown */
+	const ssMenuOnScrolldown = () => {
+
+		const menuTrigger = $('#header-menu-trigger');
+
+		$WIN.on('scroll', () => {
+
+			if ($WIN.scrollTop() > 150) {
+				menuTrigger.addClass('opaque');
+			}
+			else {
+				menuTrigger.removeClass('opaque');
+			}
+
+		});
+	}
+
+	/* OffCanvas Menu */
+	const ssOffCanvas = () => {
+		const 	menuTrigger = $('#header-menu-trigger'),
+				nav = $('#menu-nav-wrap'),
+				closeButton = nav.find('.close-button'),
+				siteBody = $('body'),
+				mainContents = $('section, footer');
+
+				menuTrigger.on('click', (e) => {
+					e.preventDefault();
+					menuTrigger.toggleClass('is-clicked');
+					siteBody.toggleClass('menu-is-open');
+				});
+
+				closeButton.on('click', (e) => {
+					e.preventDefault();
+					menuTrigger.trigger('click');	
+				});
+
+				siteBody.on('click', (e) => {		
+					if( !$(e.target).is('#menu-nav-wrap, #header-menu-trigger, #header-menu-trigger span') ) {
+						menuTrigger.removeClass('is-clicked');
+						siteBody.removeClass('menu-is-open');
+					}
+				});
+	}
 })(jQuery);
